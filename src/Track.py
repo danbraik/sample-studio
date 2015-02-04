a245a3d87bc3dc2cd3ec1bebf53e77c29ccd0409
Daniel Braikeh <danbraik@hotmail.com>
2015-02-04 09:42:04 +0100

    Display signal visualization

--- src/Track.py
+++ src/Track.py
@@ -3,7 +3,7 @@ import Color
 import DrawText
 
 WIDTH = 600
-HEIGHT = 50
+HEIGHT = 143
 
 
 class Track:
@@ -13,6 +13,10 @@ class Track:
 		self._name = soundpath.split("/")[-1]
 		self._is_selected = False
 		self._nb_playing = 0
+		self._img = None
+	
+	def set_imagepath(self, path):
+		self._img = pygame.image.load(path)
 
 	def get_name(self):
 		return self._name
@@ -41,15 +45,23 @@ class Track:
 
 	
 	def draw(self, surface, num=0):
+		w_num = 20
+		x_title = w_num + 5
+		surface.fill(Color.darkBlue)
 		# num
 		num_col = Color.bg_selected_track if self.is_selected() else Color.bg_track
-		pygame.draw.rect(surface, num_col, (0,0, 20,HEIGHT), 0)
-		DrawText.draw(surface, str(num), 5, 25)
+		pygame.draw.rect(surface, num_col, (0,0, w_num, HEIGHT), 0)
+		DrawText.draw(surface, str(num), 5, HEIGHT/2)
+		# visualization
+		if self._img:
+			surface.blit(self._img, (w_num, 0, WIDTH, 147))
 		# title
-		pygame.draw.rect(surface, Color.darkBlue, (20,0,WIDTH-20,HEIGHT), 0)
-		DrawText.draw(surface, self.get_name(), 25, 25)
+		# pygame.draw.rect(surface, Color.darkBlue, (20,0,WIDTH-20,HEIGHT), 0)
+		DrawText.draw(surface, self.get_name(), x_title, 5)
 		# status
-		stat_col = Color.yellow if self.is_playing() else Color.bg_track
-		pygame.draw.rect(surface, stat_col, (500, 0,WIDTH-500,HEIGHT), 0)
+		if self.is_playing():
+			w_r = 2
+			pygame.draw.rect(surface, Color.yellow, (w_num, 0, WIDTH-w_num-1, HEIGHT-1), w_r)
+
 
 
