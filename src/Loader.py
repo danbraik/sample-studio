a4b634a1920f66d6fb9b833c807d45761157979d
Daniel Braikeh <danbraik@hotmail.com>
2015-02-04 09:42:30 +0100

    Load sound visualization

--- src/Loader.py
+++ src/Loader.py
@@ -1,12 +1,16 @@
-import Track, Manager
+import Track, Manager, os
 
 def load_playlist(manager, playlist_filename):
 	print "Loading playlist '" + playlist_filename + "'"
 	with open(playlist_filename, "r") as f:
 		content = f.readlines()
 		for l in content:
+			l = l.rstrip()
 			try:
-				track = Track.Track(l.rstrip())
+				track = Track.Track(l)
+				image = l + ".bmp"
+				if os.path.exists(image):
+					track.set_imagepath(image)
 				manager.add(track)				
 			except RuntimeError:
 				print "Error when loading sound '" + l + "'"
